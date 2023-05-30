07d586044b22f55ed1d9f39d51f04f0f745698a4
oscar1098 <oscarardila1098@gmail.com>
2023-05-29 19:01:21 -0500

    On branch main  Changes to be committed:

--- javaScript/numerosAmigos/app.js
+++ javaScript/numerosAmigos/app.js
@@ -1,10 +1,10 @@
 let num,suma;
 
 function divisores (numero){
-    suma = 0
+    suma = 0;
     for(let i = 1; i< numero; i++){
         if ( numero%i == 0){
-            suma+= i
+            suma+= i;
         }
     }
     return suma;
@@ -15,13 +15,13 @@ function numAmigos(num1,num2){
     let amigo2 = divisores(num2);
 
     if (amigo1 == num2 && amigo2 == num1){
-        document.write(`${num1} y ${num2} son amigos`)
+        document.write(`${num1} y ${num2} son amigos`);
     }else{
-        document.write(" No son amigos")
+        document.write(" No son amigos");
     }
 }
 
 let num1 = parseInt(prompt("Ingrese el numero 1"));
 let num2 = parseInt(prompt("Ingrese el numero 2"));
 
-numAmigos(num1,num2)
+numAmigos(num1,num2);
