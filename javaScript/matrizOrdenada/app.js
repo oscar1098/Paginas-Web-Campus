9b3e60bd861a11819cf1b522ee726f4134dca1f5
oscar1098 <oscarardila1098gmail.com>
2023-05-30 23:41:37 -0500

    On branch main  Changes to be committed:

---
+++ javaScript/matrizOrdenada/app.js
@@ -0,0 +1,60 @@
+const crearMatriz = (tamañoMatriz) => {
+    let matriz = [];
+    for (let i = 1; i <= tamañoMatriz; i++){
+        let numero = [];
+        for ( let j = 1; j<=tamañoMatriz; j++){
+            numero.push(Math.floor( Math.random() * 100 ));
+        }
+        matriz.push(numero);
+    }
+    console.log(matriz);
+
+    return matriz;
+}
+
+const matrizOrdenada = (tamañoMatriz) => {
+
+    let matrizFinal = [];
+    let indiceInicial = 0, indiceFinal = 0;
+    const arregloTotal = crearMatriz(tamañoMatriz).flat().sort( (a,b) => a-b );
+
+    for ( let i = 1; i <= tamañoMatriz; i++ ){
+        
+        indiceFinal = indiceInicial + tamañoMatriz;
+
+        if ( i % 2 === 0 ){
+            matrizFinal.push( arregloTotal.slice( indiceInicial, indiceFinal ).reverse() );
+        }else{
+            matrizFinal.push( arregloTotal.slice( indiceInicial, indiceFinal ) );
+        }
+
+        indiceInicial = indiceFinal;
+    }
+
+    return matrizFinal;
+    
+}
+
+console.log(matrizOrdenada(4));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
