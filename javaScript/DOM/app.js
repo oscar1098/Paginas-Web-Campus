494ce2d94499e27837f1c3c325c0a3a207cd011b
oscar1098 <oscarardila1098gmail.com>
2023-06-01 00:45:57 -0500

    On branch main  Changes to be committed:

--- javaScript/DOM/app.js
+++
@@ -1,5 +0,0 @@
-const h1 = document.createElement('h1');
-h1.innerText = 'Hola';
-h1.style.display = 'none'
-
-document.body.append(h1)
--- javaScript/DOM/propinas/script.js
+++ javaScript/DOM/propinas/script.js
@@ -1,27 +1,30 @@
 // Capturar el formulario y los elementos relevantes
-const form = document.getElementById('calculator-form'),
+const form = document.getElementById('calculator-form');
+      personas = document.querySelector('#personas')
       billInput = document.getElementById('bill'),
       tipSelect = document.getElementById('tip'),
       resultDiv = document.getElementById('result'),
       billAmountSpan = document.getElementById('bill-amount'),
       tipPercentageSpan = document.getElementById('tip-percentage'),
       tipAmountSpan = document.getElementById('tip-amount'),
-      totalAmountSpan = document.getElementById('total-amount'),
-      nmroPersonas = document.querySelector('#nmroPersonas');
+      totalAmountSpan = document.getElementById('total-amount');
 
 // Escuchar el evento 'submit' del formulario
 form.addEventListener('submit', function(event) {
   event.preventDefault(); // Evitar el envío del formulario
   
   // Validar los valores ingresados
-  const bill = parseFloat(billInput.value),
-        tipPercentage = parseFloat(tipSelect.value);
+  const bill = parseFloat(billInput.value);
+  const tipPercentage = parseFloat(tipSelect.value);
   
-  (bill <= 0) && alert('Por favor, ingresa valores numéricos válidos para el monto de la cuenta y el porcentaje de propina.');
-
+  if (isNaN(bill) || isNaN(tipPercentage) || bill <= 0) { 
+    alert('Por favor, ingresa valores numéricos válidos para el monto de la cuenta y el porcentaje de propina.');
+    return;
+  }
+  
   // Calcular la propina y el monto total
-  const tipAmount = bill * tipPercentage,
-        totalAmount = (bill + tipAmount) / parseFloat(nmroPersonas.value);
+  const tipAmount = bill * tipPercentage;
+  const totalAmount = (bill + tipAmount) / parseFloat( personas.value );
   
   // Mostrar los resultados
   billAmountSpan.textContent = bill.toFixed(2);
@@ -29,5 +32,5 @@ form.addEventListener('submit', function(event) {
   tipAmountSpan.textContent = tipAmount.toFixed(2);
   totalAmountSpan.textContent = totalAmount.toFixed(2);
   
-  resultDiv.classList.remove('hiddenn');
+  resultDiv.classList.remove('hidden');
 });
