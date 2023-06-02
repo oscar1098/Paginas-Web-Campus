00baf3df073b15104d6e819876da29b5d592df00
oscar1098 <oscarardila1098gmail.com>
2023-06-02 00:46:25 -0500

    On branch main  Changes to be committed:

---
+++ javaScript/DOM/directorio empresarial/app.js
@@ -0,0 +1,50 @@
+const agregar = document.getElementById('agregar'),
+      nombre = document.getElementById('nombre'),
+      email = document.getElementById('email'),
+      telefono = document.getElementById('telefono'),
+      botonEliminar = document.getElementById('botonEliminar'),
+      idEliminar = document.getElementById('idEliminar');
+      padreTabla = document.getElementById('padreTabla');
+
+let num = 1;
+
+agregar.addEventListener('submit',function(event){
+
+    event.preventDefault();
+
+    const hijo = document.createElement('tr'),
+          tagth = document.createElement('th'),
+          tagtdNombre = document.createElement('td'),
+          tagtdEmail = document.createElement('td'),
+          tagtdTelefono = document.createElement('td');
+    
+    hijo.classList.add('table-secondary', 'tabla');
+    hijo.id = num;
+
+    tagth.textContent = `${num}`;
+    tagtdNombre.textContent =`${nombre.value}`;
+    tagtdTelefono.textContent = `${telefono.value}`;
+    tagtdEmail.textContent = `${email.value}`;
+
+    hijo.appendChild(tagth);
+    hijo.appendChild(tagtdNombre);
+    hijo.appendChild(tagtdTelefono);
+    hijo.appendChild(tagtdEmail);
+
+
+    padreTabla.appendChild(hijo);
+
+    nombre.value = '';
+    email.value = '';
+    telefono.value = '';
+
+    num++;
+})
+
+botonEliminar.addEventListener('click', function() {
+
+    let hijoEliminar = document.getElementById(idEliminar.value);
+    padreTabla.removeChild(hijoEliminar);
+
+    idEliminar.value = '';
+})
