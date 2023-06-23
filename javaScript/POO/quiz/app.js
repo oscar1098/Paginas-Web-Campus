5773b64790fe3b721ae454adff131e76c1afc780
oscar1098 <oscarardila1098@gmail.com>
2023-06-22 21:29:05 -0500

    On branch main  Changes to be committed:

--- javaScript/POO/quiz/app.js
+++ javaScript/POO/quiz/app.js
@@ -17,6 +17,10 @@ class Quiz {
     addQuiz (quiz) {
         this.quices.push(quiz);
     }
+
+    get getQuiz() {
+        return this.quices;
+    }
 }
 
 const quices =  new Quiz();
@@ -28,16 +32,55 @@ const resB = document.getElementById('resB');
 const resC = document.getElementById('resC');
 const correcta = document.getElementById('correcta');
 const listaQuices = document.getElementById('listaQuices');
+const enviaresp = document.getElementById('enviaresp');
+const enviarespuestas = document.getElementById('enviarespuestas');
+let controlador = 0;
 
 const agregar = (e) => {
     e.preventDefault();
 
     const infoQuiz = new InfoQuiz( pregunta.value, resA.value, resB.value, resC.value, correcta.value );
-
     quices.addQuiz(infoQuiz);
-    console.log(quices);
+    formulario.reset();
+    listarQuices();
 }
  
+const listarQuices = () => {
+    
+    const itemsLista = document.createElement('li');
+    itemsLista.classList.add('mb-4')
+    for ( let i = 0;  i< quices.getQuiz.length; i++ ){
+        const { pregunta, opcionA, opcionB, opcionC } = quices.getQuiz[i];
+
+        itemsLista.innerHTML = `
+            <p>${pregunta} </p>
+            <div class = 'd-block'>
+                <input type="radio" name="${controlador}"  id= 'opcionA'>
+                <label for="radio">A. ${opcionA}</label>
+            </div>
+            <div class = 'd-block'>
+                <input type="radio" name="${controlador}"  id= 'opcionB'>
+                <label for="radio">B. ${opcionB}</label>
+            </div>
+            <div class = 'd-block'>
+                <input type="radio" name="${controlador}"  id= 'opcionC'>
+                <label for="radio">C. ${opcionC}</label>
+            </div>
+        `
+    }
+
+    listaQuices.appendChild(itemsLista);
+    enviaresp.classList.remove('ocultar');
+    controlador++
+}
+
+const revisar = (e) => {
+    const inputs = listaQuices.querySelectorAll('input[type="radio"]:checked');
+    e.preventDefault();
+    console.log(inputs);
+}
+
 formulario.addEventListener('submit',agregar)
+enviarespuestas.addEventListener('submit', revisar)
 
 
