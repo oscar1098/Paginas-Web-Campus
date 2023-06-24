0b4ed9fde9f01c80acf9bd20183d76d3e93cc134
oscar1098 <oscarardila1098gmail.com>
2023-06-24 11:31:09 -0500

    On branch main  Changes to be committed:

--- javaScript/POO/quiz/app.js
+++ javaScript/POO/quiz/app.js
@@ -21,6 +21,15 @@ class Quiz {
     get getQuiz() {
         return this.quices;
     }
+
+    editQuiz(index, newEdit) {
+      
+        this.quices.splice( index, 1, newEdit );
+    }
+
+    eliminar (index){
+        this.quices.splice(index,1);
+    }
 }
 
 const quices =  new Quiz();
@@ -36,6 +45,13 @@ const enviaresp = document.getElementById('enviaresp');
 const enviarespuestas = document.getElementById('enviarespuestas');
 let controlador = 0;
 
+const formularioEdit = document.getElementById('formularioEdit');
+const preguntaEdit = document.getElementById('preguntaEdit');
+const resAEdit = document.getElementById('resAEdit');
+const resBEdit = document.getElementById('resBEdit');
+const resCEdit = document.getElementById('resCEdit');
+const correctaEdit = document.getElementById('correctaEdit');
+
 const agregar = (e) => {
     e.preventDefault();
 
@@ -46,41 +62,125 @@ const agregar = (e) => {
 }
  
 const listarQuices = () => {
-    
-    const itemsLista = document.createElement('li');
-    itemsLista.classList.add('mb-4')
-    for ( let i = 0;  i< quices.getQuiz.length; i++ ){
-        const { pregunta, opcionA, opcionB, opcionC } = quices.getQuiz[i];
 
+    listaQuices.innerHTML = '';
+    
+    for ( let i = 0;  i< quices.getQuiz.length; i++ ){
+        
+        const itemsLista = document.createElement('li');
+        itemsLista.classList.add('mb-4')
+        
+        const { pregunta, opcionA, opcionB, opcionC } = quices.getQuiz[i];
         itemsLista.innerHTML = `
             <p>${pregunta} </p>
             <div class = 'd-block'>
-                <input type="radio" name="${controlador}"  id= 'opcionA'>
+                <input type="radio" name="${controlador}"  value = 'A'>
                 <label for="radio">A. ${opcionA}</label>
             </div>
             <div class = 'd-block'>
-                <input type="radio" name="${controlador}"  id= 'opcionB'>
+                <input type="radio" name="${controlador}"  value = 'B'>
                 <label for="radio">B. ${opcionB}</label>
             </div>
             <div class = 'd-block'>
-                <input type="radio" name="${controlador}"  id= 'opcionC'>
+                <input type="radio" name="${controlador}"  value = 'C'>
                 <label for="radio">C. ${opcionC}</label>
             </div>
+            <div class = 'text-start mt-5'>
+                <button type="button" data-bs-toggle="modal" data-bs-target='#modalEditar' class = 'btn btn-outline-warning me-3'  onclick="editar(${i})">Editar</button>
+                <button type="button" class = 'btn btn-outline-danger'  onclick="eliminar(${i})">Eliminar</button>
+            </div>
         `
+        listaQuices.appendChild(itemsLista);
+        controlador++
     }
 
-    listaQuices.appendChild(itemsLista);
     enviaresp.classList.remove('ocultar');
-    controlador++
 }
+
+const eliminar = (index) => {
+    quices.eliminar(index);
+    listarQuices();
+    if ( quices.getQuiz.length === 0 ) {  enviaresp.classList.add('ocultar');  }
+}
+
+const editar = (index) => {
+
+    const { pregunta, opcionA, opcionB, opcionC, correcta  } = quices.getQuiz[index];
+   
+    preguntaEdit.value = pregunta;
+    resAEdit.value = opcionA;
+    resBEdit.value = opcionB;
+    resCEdit.value = opcionC;
+    correctaEdit.value = correcta;
+
+    const editarQuiz = (e) => {
+        e.preventDefault();
+        console.log(index);
+        const editQuiz = new InfoQuiz ( preguntaEdit.value, resAEdit.value, resBEdit.value, resCEdit.value, correctaEdit.value );
+        quices.editQuiz(index,editQuiz);
+        listarQuices();
+        formularioEdit.removeEventListener('submit', editarQuiz);
+    }
+
+    formularioEdit.addEventListener('submit', editarQuiz);
+}
+
 
 const revisar = (e) => {
-    const inputs = listaQuices.querySelectorAll('input[type="radio"]:checked');
+
+    let correctas = 0;
+    
     e.preventDefault();
-    console.log(inputs);
+
+    const inputs = listaQuices.querySelectorAll('input[type="radio"]');
+
+    for ( let i = 0; i < inputs.length; i++ ) {
+
+        let name = inputs[i].name;
+
+        let radioSelec = document.querySelectorAll(`input[type='radio'][name='${name}']:checked`);
+
+        if ( radioSelec.length === 0 ) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error de envio',
+                text: 'Debe  completar todo el formulario para enviar',
+              })
+            return;
+        }
+    }
+
+    let respuestas = document.querySelectorAll(`input[type='radio']:checked`);
+    nmroPreguntas = respuestas.length;
+
+    for ( let i = 0; i < respuestas.length; i++ ){
+
+        if ( quices.getQuiz[i]['correcta'] === respuestas[i].value  ){
+            correctas++;
+        }
+
+    }
+
+    if ( correctas > nmroPreguntas*0.60 ){
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Aprobado',
+            text: `Su puntaje fue ${correctas}/${nmroPreguntas}`,
+          })
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Reprobado',
+            text: `Su puntaje fue ${correctas}/${nmroPreguntas}`,
+          })
+    }
+    
+
 }
 
-formulario.addEventListener('submit',agregar)
-enviarespuestas.addEventListener('submit', revisar)
+formulario.addEventListener('submit',agregar);
+enviarespuestas.addEventListener('submit', revisar);
+
 
 
