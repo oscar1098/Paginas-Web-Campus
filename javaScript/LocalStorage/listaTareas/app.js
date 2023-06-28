ff6e1ab173679d7b31faf9d681e22cb4ca29d579
oscar1098 <oscarardila1098gmail.com>
2023-06-28 01:58:51 -0500

    On branch main  Changes to be committed:

--- javaScript/LocalStorage/listaTareas/app.js
+++ javaScript/LocalStorage/listaTareas/app.js
@@ -1,134 +1,84 @@
 let articulos = JSON.parse(localStorage.getItem('cartItems')) || [];
-
+console.log( typeof articulos);
 const agregar = document.getElementById('agregar');
 const articulo = document.getElementById('articulo');
 const precio = document.getElementById('precio');
 const vaciar = document.getElementById('vaciar');
 const listaArt = document.getElementById('listaArt');
 
-class carrito {
 
-    agregarArticulo( articulo,precio ) {
-        const item = {
-            nombre : articulo,
-            precio : precio
-        } 
-        articulos.push( item )
-        localStorage.setItem('articulos', JSON.stringify(articulos));
+class Item {
+
+    constructor( nombre, precio ){
+        this.nombre = nombre;
+        this.precio = precio;
     }
 
-    
-    mostrarItem () {
-        
-        if ( articulos.length == 0 ) { listaArt.innerHTML = ''; return }
+    crearArt () {
 
-        for ( let i = 0; i < articulos.length; i++ ) {
-            
-            const li = document.createElement('li');
-
-            li.innerHTML = `
-            <p>Articulo: ${articulos[i].nombre}</p>
-            <p>Precio: ${articulos[i].precio}</p>
-            <button class="btn btn-outline-danger" onclick="${this.eliminarArt(i)}" >Eliminar</button>
-            `
-            listaArt.appendChild(li);
+        return {
+            nombre : this.nombre,
+            precio : this.precio
         }
     }
-    
-    eliminarArt (i) {
-        articulos.splice(i,1);
-        localStorage.setItem('articulos', JSON.stringify(articulos));
-        this.mostrarItem();
+}
+
+class Carrito {
+
+
+    setArticulo(nombreLocalStorage,item) {
+        articulo.push(item);
+        localStorage.setItem(nombreLocalStorage, JSON.stringify(articulo));
     }
+
+    eliminarItem(i,nombreLocalStorage){
+        articulo.splice(i,1);
+        localStorage.setItem(nombreLocalStorage, JSON.stringify(articulo));
+    }
+
+    vaciarArticulos (nombreLocalStorage) {
+        localStorage.removeItem('cartItems');
+    }
+}
+
+
+const carrito = new Carrito( articulos );
+
+const agregarArticulo = (e) => {
+
+    e.preventDefault();
+    const item = new Item( articulo.value, precio.value );
+    carrito.setArticulo('cartItems', item.crearArt());
+    mostratArticulos();
+    agregar.reset();
     
 }
 
-const carritasd =  new carrito(articulos);
-
-carritasd.agregarArticulo('saddsdfs',2133423);
-carritasd.mostrarItem();
-
-
-
-
-// class Articulo {
-
-//     constructor ( nombre, precio ) {
-//         this.nombre = nombre;
-//         this.precio = precio;
-//     }
-
-//     agregarArticulo() {
-//         this.articulo = {
-//             nombre : this.articulo,
-//             precio : this.precio
-//         }
-
-//         return this.articulo;
-//     }
-// }
-
-// class OperarArticulos {
-
-//     constructor ( articulo ) {
-//         this.articulo = articulo;
-//         this.articulos = [];
-//     }
+const mostratArticulos =() => {
     
-
-//     getArticulo () {
-//         if ( localStorage.getItem('articulos') ) { 
-//             this.articulos = JSON.parse( localStorage.getItem('articulos') );
-//          }
-//     }
-
-//     agregarLocalStorage () {
-
-//         this.getArticulo();
-//         this.articulos.push(this.articulo)
-//         localStorage.setItem( 'articulos', JSON.stringify( this.articulos ) )
-//     }
-
-//     eliminarArt (i) {
-
-//         this.getArticulo();
-//         this.articulos.splice(i,1);
-//         localStorage.setItem( 'articulos', JSON.stringify( this.articulos ) );
-//         this.mostrarArticulos();
-
-//     }
-
-//     mostrarArticulos () {
-
-//         this.getArticulo();
-//         listaArt.innerHTML ='';
-        
-//         for ( let i = 0; i < this.articulos.length; i++ ) {
-            
-//             const li = document.createElement('li');
-
-//             li.innerHTML = `
-//             <p>Articulo: ${this.articulos[i].nombre}</p>
-//             <p>Precio: ${this.articulos[i].precio}</p>
-//             <button class="btn btn-outline-danger" onclick="${this.eliminarArt(i)}">Eliminar</button>
-//             `
-//             listaArt.appendChild(li);
-//         }
-
-//         console.log(this.articulos);
-//     }
-// }
-
-
-
-// const eventoForm = (e) => {
+    listaArt.innerHTML = '';
+    for ( let i = 0; i < articulos.length; i++ ){
     
-//     e.preventDefault();
+        const li = document.createElement('li');
     
-//     const art = new Articulo( articulo.value, precio.value );
-//     const articulos = new OperarArticulos(art);
-//     articulos.agregarLocalStorage();
-//     articulos.mostrarArticulos();
-// }
+        li.innerHTML = `
+            <p>${articulos[i].nombre}</p>
+            <p>${articulos[i].precio}</p>
+            <button class="btn btn-outline-danger" onclick="eliminarArt(i)">Eliminar</button>
+        `
+        listaArt.appendChild(li);
+    }
+}
 
-// agregar.addEventListener( 'submit', eventoForm )
+const limpiarLista = () => {
+    carrito.vaciarArticulos('cartItems');
+    console.log(articulos);
+    mostratArticulos();
+}
+
+
+mostratArticulos();
+agregar.addEventListener('submit', agregarArticulo )
+vaciar.addEventListener('click', limpiarLista);
+
+
