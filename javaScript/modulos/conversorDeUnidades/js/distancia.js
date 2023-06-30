a331761e19b24a5ce290c41c8c5a1f8494118265
oscar1098 <oscarardila1098gmail.com>
2023-06-30 16:23:22 -0500

    On branch main  Changes to be committed:

--- javaScript/modulos/conversorDeUnidades/js/distancia.js
+++ javaScript/modulos/conversorDeUnidades/js/distancia.js
@@ -1,2 +1,59 @@
 
-export const conversorDistancia = (m) => m * 3.2808
+const km = {
+
+    m   :   (km) =>  km * 1000 ,
+    cm  :   (km) => km * 100000,  
+    mm  :   (km) => km * 1000000,  
+    mi  :   (km) => km * 0.621371,  
+    yd  :   (km) => km * 1093.61,  
+    ft  :   (km) => km * 3280.84,  
+    in  :   (km) => km * 39370.1  
+
+}
+
+const m = {
+
+    km   :  (m) => m * 0.001 ,
+    cm  :   (m) => m * 100,  
+    mm  :   (m) => m * 1000,  
+    mi  :   (m) => m * 0.000621371,  
+    yd  :   (m) => m * 1.09361,  
+    ft  :   (m) => m * 3.28084,  
+    in  :   (m) => m * 39.3701  
+
+}
+
+
+const mm = {
+
+    m   :   (mm) => mm * 0.001 ,
+    cm  :   (mm) => mm * 0.1,  
+    km  :   (mm) => mm * 0.000001,  
+    mi  :   (mm) => mm * 6.2137,  
+    yd  :   (mm) => mm * 0.00109361,  
+    ft  :   (mm) => mm * 0.00328084,  
+    in  :   (mm) => mm *  0.0393701  
+
+}
+
+const mi = {
+
+    km  :   (mi) => mi * 1.60934,  
+    m   :   (mi) => mi * 1609.34 ,
+    cm  :   (mi) => mi * 160934,  
+    mm  :   (mi) => mi * 1609340,  
+    yd  :   (mi) => mi * 1760,  
+    ft  :   (mi) => mi * 5280,  
+    in  :   (mi) => mi * 63360  
+
+}
+
+
+
+
+
+
+
+
+
+
