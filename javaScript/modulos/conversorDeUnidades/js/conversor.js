c9f551d66b98702d4032b7092a0c765c9a5587c1
oscar1098 <oscarardila1098gmail.com>
2023-07-01 19:12:39 -0500

    On branch main  Changes to be committed:

--- javaScript/modulos/conversorDeUnidades/js/conversor.js
+++ javaScript/modulos/conversorDeUnidades/js/conversor.js
@@ -1,48 +1,89 @@
-import { conversorDistancia } from "./distancia.js";
-import { convertirPeso } from "./peso.js";
-import { convertirTemperatura } from "./temperatura.js";
+import { distancia } from "./distancia.js";
+import { peso } from "./peso.js";
+import { temperatura } from "./temperatura.js";
 
 
-const libras = document.getElementById('libras');
-const kilogramos = document.getElementById('kilogramos');
-const peso = document.getElementById('peso');
-const distancia = document.getElementById('distancia');
-const metros = document.getElementById('metros');
-const pies = document.getElementById('pies');
-const temperatura = document.getElementById('temperatura');
-const celius = document.getElementById('celius');
-const fahrenheit = document.getElementById('fahrenheit');
+const selecM = document.getElementById('selecM');
+const selecMC = document.getElementById('selecMC');
+const masaInput = document.getElementById('masaInput');
+const masaCalcu = document.getElementById('masaCalcu');
+const masaResul = document.getElementById('masaResul');
+const selecD = document.getElementById('selecD');
+const selecDC = document.getElementById('selecDC');
+const distanciaInput = document.getElementById('distanciaInput');
+const distanciaCalcu = document.getElementById('distanciaCalcu');
+const distanciaResul = document.getElementById('distanciaResul');
+const selecT = document.getElementById('selecT');
+const selecTC = document.getElementById('selecTC');
+const TemperaturaInput = document.getElementById('TemperaturaInput');
+const temperaturaCalcu = document.getElementById('temperaturaCalcu');
+const temperaturaResul = document.getElementById('temperaturaResul');
 
 
-const calcuPeso = () => {
-    if ( convertirPeso( kilogramos.value) >= 0 ){
+const crearPrimerSelect = (objeto, selec) => {
+    const keys = Object.keys(objeto);
 
-        libras.value = `${convertirPeso( kilogramos.value)}`;
-    } else {
-        alert('El valor debe ser positivo');
-        libras.value = '';
-        kilogramos.value = '';
+    keys.forEach( ( unidad ) => {
+        const option = document.createElement('option');
+        option.textContent = unidad;
+        selec.appendChild(option) 
+    } )
+} 
 
+const crearSegundSelect = ( event, objeto, selecS ) => {
+
+    if (  event.target.value == 'Seleccione' ) {
+        selecS.innerHTML = '';
+        return;
     }
+
+    const key = event.target.value;
+
+    selecS.innerHTML = '';
+
+    const unidades = Object.keys( objeto[key] );
+
+
+    unidades.forEach( ( unidad ) => {
+        const option = document.createElement('option');
+        option.textContent = unidad;
+        selecS.appendChild( option );
+    } ) 
 }
 
-const calcuDis = () => {
+const calculos = ( objeto, key1, key2, inputO,inputR ) => {
 
-    if ( conversorDistancia( metros.value) >= 0 ){
+    try {
 
-        pies.value = `${conversorDistancia( metros.value)}`;
-    } else {
-        alert('El valor debe ser positivo');
-        pies.value = '';
-        metros.value = '';
-
+        inputR.value =  objeto[key1][key2]( parseFloat( inputO.value ) )  ;
+    } catch {
+        alert('Operacion Invalida')
     }
+
+
 }
 
-const calcuTemp = () => {
-    fahrenheit.value = convertirTemperatura( celius.value );
-}
 
-peso.addEventListener('click', calcuPeso );
-distancia.addEventListener('click', calcuDis );
-temperatura.addEventListener('click', calcuTemp );
+
+crearPrimerSelect( distancia, selecD );
+crearPrimerSelect( peso, selecM );
+crearPrimerSelect( temperatura, selecT );
+
+selecD.addEventListener( 'change', (event) => crearSegundSelect( event, distancia, selecDC )  ) ;
+selecM.addEventListener( 'change', (event) => crearSegundSelect( event, peso, selecMC )  ) ;
+selecT.addEventListener( 'change', (event) => crearSegundSelect( event, temperatura, selecTC )  ) ;
+distanciaCalcu.addEventListener( 'click', () => calculos( distancia, selecD.value, selecDC.value, distanciaInput, distanciaResul ) )
+masaCalcu.addEventListener( 'click', () => calculos( peso, selecM.value, selecMC.value, masaInput, masaResul ) )
+temperaturaCalcu.addEventListener( 'click', () => calculos( temperatura, selecT.value, selecTC.value, TemperaturaInput, temperaturaResul ) )
+
+
+
+
+
+
+
+
+
+
+
+
--- javaScript/modulos/conversorDeUnidades/js/temperatura.js
+++ javaScript/modulos/conversorDeUnidades/js/temperatura.js
@@ -1,2 +1,18 @@
 
-export const convertirTemperatura = (c) =>  (c * 9/5) + 32
+
+export const temperatura = {
+c : {
+    f : (c) =>  ( c * 9/5 ) + 32,
+    k : (c) =>  ( c + 273.15 ),
+},
+
+f : {
+    c :  (f) => (f - 32) * 5/9,
+    k : (f) => (f + 459.67) * 5/9,
+},
+
+k : {
+    c : (k) =>  k - 273.15,
+    f : (k) =>  (k * 9/5) - 459.67
+}
+}
